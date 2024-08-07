f76bc28b8003fdb8f82248f500d9c284f3b5195f
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-08-07 23:50:31 +0530

    Add files via upload

---
+++ day6.js
@@ -0,0 +1,12 @@
+let x=-"1"
+console.log(typeof x)
+let y=Boolean(x);
+console.log(y)
+console.log(typeof y)
+
+// let x=+"1";
+// let x=+"0";
+// let x=+"hi";
+// let x="hi";
+// let y=Boolean(x);
+// console.log(y);
