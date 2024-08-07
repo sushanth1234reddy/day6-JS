623e83aabf802b4941b643657f8657695d4045c8
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-08-07 23:51:29 +0530

    Add files via upload

---
+++ prog1.js
@@ -0,0 +1,22 @@
+//prog 1
+// onetime view msg
+// if the msg is onetimeview  then we can't forward it to another frnds
+//if not , we can forward to amother frnd
+// let OneTimeMsg=true;
+// if(OneTimeMsg){
+//     console.log("can't forward it to another frnds");
+// }
+// else{
+//     console.log("can forward it to another frnds");
+// }
+
+// let onetime=Boolean(prompt("enter true or false"));
+let x=1;
+let onetime=Boolean(x);
+if(onetime){
+    console.log("can't forward it to another frnds");
+}
+else{
+    console.log("can forward it to another frnds");
+}
+
