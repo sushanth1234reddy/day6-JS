bf30e48ec98ffe484bb5e227e0d6cdca6391b8a4
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-08-07 23:51:49 +0530

    Add files via upload

---
+++ prog2.js
@@ -0,0 +1,22 @@
+//prog 2
+//msg delete option
+//if time is <24hrs then show delete for me and delete for everyone
+//if time is >24hrs then show only delete for me option
+
+//terminal method
+// let Time=18;
+// if(Time<24){
+//     console.log(" show delete for me & delete for everyone ");
+// }
+// else{
+//     console.log("show delete for me");
+// }
+
+let TIME=+prompt("enter number");
+if(TIME<24){
+    console.log(" show delete for me & delete for everyone ");
+}
+else{
+    console.log("show delete for me");
+    
+}
