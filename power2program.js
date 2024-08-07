73ecd9558880dff48438d968bd54fc12f42930ef
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-08-07 23:49:27 +0530

    Add files via upload

---
+++ power2program.js
@@ -0,0 +1,42 @@
+// let l=+prompt("enter L value:")
+// let ans=2**l;
+// alert("answer is "+ans);
+
+//2**n=value
+//2=value**(1/n)
+
+// let x=128;
+// let y=7;
+// let ans=x**(1/y);
+// if(ans==2){
+// console.log("value is power of 2")
+// }
+// else{
+//     console.log("Value is not a power of 2");
+// }
+// let x=64;
+
+//using if-else
+
+let n=prompt("enter n value");
+if((n&(n-1))==0){
+    console.log("power of 2 YES")
+}
+else{
+    console.log("NO")
+}
+
+//using for loop
+// let x=127
+// ;
+//        for(let i=0; i<10; i++){
+//         let ans=x**(1/i);
+//         if(ans==2){
+//             console.log(x+" qnumber is power of 2");
+//         }
+//         else{
+//             alert("number is not")
+//         }
+//        }
+       
+
